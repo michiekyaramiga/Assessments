095b5d5f0579f976f5be74298dc8e49215969528
michiekyaramiga <michiekyaramiga@gmail.com>
2021-12-14 08:47:32 -0800

    Add files via upload

---
+++ Heuristics.TechEval-master/Core/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Heuristics.TechEval.Core.Models;
+
+namespace Heuristics.TechEval.Core {
+
+	public class DataContext : DbContext {
+
+		public DataContext() : base("Database") { }
+
+		public DbSet<Member> Members { get; set; }
+		public DbSet<Category> Categories { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		}
+	}
+}
