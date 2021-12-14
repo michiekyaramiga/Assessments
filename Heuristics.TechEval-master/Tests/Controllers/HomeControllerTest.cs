6048d5676c7cdfc5d8573d0a11a9c62911b5f4f0
michiekyaramiga <michiekyaramiga@gmail.com>
2021-12-14 08:56:42 -0800

    Add files via upload

---
+++ Heuristics.TechEval-master/Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Heuristics.TechEval.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Heuristics.TechEval.Tests.Controllers {
+
+	[TestClass]
+	public class HomeControllerTest {
+
+		[TestMethod]
+		public void Index() {
+			// Arrange
+			HomeController controller = new HomeController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+	}
+}
